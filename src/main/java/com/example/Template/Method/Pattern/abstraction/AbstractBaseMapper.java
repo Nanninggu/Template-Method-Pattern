34b02f154739a1a63a9cf56d00698f9eb0e5e688
김승현 <pj.seungkim@kalmate.net>
2024-05-29 22:45:18 +0900

    commit-002

--- src/main/java/com/example/Template/Method/Pattern/abstraction/AbstractBaseMapper.java
+++ src/main/java/com/example/Template/Method/Pattern/abstraction/AbstractBaseMapper.java
@@ -2,6 +2,9 @@ package com.example.Template.Method.Pattern.abstraction;
 
 import java.util.List;
 
+/** 해당 인터페이스가 이 프로젝트에서
+ * 템플릿 메소드의 역할을 수행한다. **/
+
 public interface AbstractBaseMapper<T> {
     default List<T> selectOne(String id) {
         List<T> result = executeSelectOne(id);
